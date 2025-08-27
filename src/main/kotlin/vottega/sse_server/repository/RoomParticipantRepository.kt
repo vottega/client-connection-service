6edecccfae2d854fde4ed8f1a6dd2f59f37e67da
minguni <80194344+min-guni@users.noreply.github.com>
2025-08-27 23:58:25 +0900

    fix : concurrentHashMap 로직 compute 안에서 모두 해결하도록 변경

--- src/main/kotlin/vottega/sse_server/repository/RoomParticipantRepository.kt
+++ src/main/kotlin/vottega/sse_server/repository/RoomParticipantRepository.kt
@@ -2,9 +2,7 @@ package vottega.sse_server.repository
 
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
-import reactor.core.scheduler.Schedulers
 import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.CopyOnWriteArraySet
 
 @Repository
 class RoomParticipantRepository {
@@ -13,18 +11,16 @@ class RoomParticipantRepository {
 
   fun addParticipant(roomId: Long, userId: Any): Mono<Set<Any>> =
     Mono.fromCallable {
-      roomParticipantMap
-        .computeIfAbsent(roomId) { CopyOnWriteArraySet() }
-        .apply { add(userId) }
-        .toSet()
-    }.subscribeOn(Schedulers.parallel())
+      roomParticipantMap.compute(roomId) { _, set ->
+        (set ?: ConcurrentHashMap.newKeySet<Any>()).apply { add(userId) }
+      }!!.toSet()
+    }
 
   fun removeParticipant(roomId: Long, userId: Any): Mono<Set<Any>> =
     Mono.fromCallable {
-      roomParticipantMap[roomId]
-        ?.apply { remove(userId) }
-        ?.takeIf { it.isNotEmpty() }
-        ?.toSet()
-        ?: emptySet()
-    }.subscribeOn(Schedulers.parallel())
+      roomParticipantMap.computeIfPresent(roomId) { _, set ->
+        set.remove(userId)
+        if (set.isEmpty()) null else set
+      }?.toSet() ?: emptySet()
+    }
 }
